ffeb9daecaf67198bfd5f7e48c021a9205c01ac6
marvellous <djmarvel@yahoo.com>
2024-09-17 17:28:47 -0400

    done

--- admin/index.php
+++ admin/index.php
@@ -45,7 +45,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><i class="fas fa-ticket-alt"></i> AUK HELP-DESK SYSTEM</a>
+            <a class="navbar-brand" href="#"><i class="fas fa-ticket-alt"></i> HELP-DESK SYSTEM</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
