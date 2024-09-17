38812900c1ef430ae44eec1a2dc60ef2e7d56514
marvellous <djmarvel@yahoo.com>
2024-09-17 14:14:44 -0400

    logo added

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include 'scripts/class_auto_loader.php';
 
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="imgs/logo.jpg" alt="" srcset="" style="width: 60px;"> AUK HELP-DESK SYSTEM</a>
+            <a class="navbar-brand" href="#"><img src="imgs/logo.jpg" alt="" srcset="" style="width: 60px;">HELP-DESK SYSTEM</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -77,7 +77,7 @@ include 'scripts/class_auto_loader.php';
 
     <nav class="navbar fixed-bottom navbar-dark">
         <div class="container justify-content-center">
-            <span style="color: white;"></span>
+            <span style="color: white;">Copyright @2024</span>
         </div>
     </nav>
 
